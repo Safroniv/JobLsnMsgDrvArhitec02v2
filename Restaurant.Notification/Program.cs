77f90c7274b5f5bb17dee8ebde6b9e3f40f32f5e
Иван Сафронов <safroniv@mail.ru>
2023-03-15 21:12:19 +0300

    JobLsnMsgDrvArhitec02v2 red01

--- Restaurant.Notification/Program.cs
+++ Restaurant.Notification/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Restaurant.Notification
+﻿using Restaurant.Notification;
+
+namespace Restaurant.Notification
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddHostedService<Worker>();
+
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.Run();
         }
     }
 }
